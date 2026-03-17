e675701559f35a3b5230204d253a744b6c2e987f
wizard <01darkofficial@gmail.com>
2026-03-17 16:27:47 +0530

    chore(dev): add script to run web and proxy servers

---
+++ scripts/dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_DIR=$(pwd)
+
+echo "Starting Web IDE..."
+cd "$ROOT_DIR/Web_IDE" && pnpm dev &
+WEB_PID=$!
+
+echo "Starting Proxy Server..."
+cd "$ROOT_DIR/Web_IDE_Proxy_Server" && pnpm dev &
+PROXY_PID=$!
+
+echo "Both services started"
+
+trap "kill $WEB_PID $PROXY_PID" EXIT
+
+wait
