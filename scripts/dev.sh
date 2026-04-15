bca243ade47fb08ae45f521b40ac79446241ac38
wizard <01darkofficial@gmail.com>
2026-04-15 10:57:56 +0530

    fix(scripts): update server paths after folder rename to restore runtime startup

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,7 +7,7 @@ cd "$ROOT_DIR/Web_IDE" && pnpm dev &
 WEB_PID=$!
 
 echo "Starting Proxy Server..."
-cd "$ROOT_DIR/Web_IDE_Proxy_Server" && pnpm dev &
+cd "$ROOT_DIR/Web_IDE_Runtime_Server" && pnpm dev &
 PROXY_PID=$!
 
 echo "Both services started"
